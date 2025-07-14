fe01b877938cf9899ca2baba0abb050b13e62173
hch507 <chunghuihan9@gmail.com>
2025-07-14 23:36:15 +0900

    create project

---
+++ cours3_ch2/src/main/java/com/example/cours3_ch2/Cours3Ch2Application.java
@@ -0,0 +1,13 @@
+package com.example.cours3_ch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours3Ch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cours3Ch2Application.class, args);
+	}
+
+}
---
+++ cours3_ch2/src/test/java/com/example/cours3_ch2/Cours3Ch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cours3_ch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours3Ch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
