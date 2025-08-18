53e423d94e70cad9d7b30c56d4bbd1f6a3c45c82
hch507 <chunghuihan9@gmail.com>
2025-08-18 22:36:39 +0900

    cours3_ch4 프로젝트 생성

---
+++ cours3_ch4/src/main/java/com/example/cours3_ch4/Cours3Ch4Application.java
@@ -0,0 +1,13 @@
+package com.example.cours3_ch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cours3Ch4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cours3Ch4Application.class, args);
+	}
+
+}
---
+++ cours3_ch4/src/test/java/com/example/cours3_ch4/Cours3Ch4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cours3_ch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours3Ch4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
